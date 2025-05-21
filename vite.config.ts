5229f868a2287f7ca6bd728378213c271da1c6ff
EndlessExplorer <meziane.tal@gmail.com>
2025-05-21 12:32:43 +0300

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  css: {
+    postcss: {
+      plugins: [
+        require('tailwindcss')(),
+        require('autoprefixer')(),
+      ],
+    },
+    devSourcemap: true, // Active les sourcemaps pour voir le fichier traité
+  },
+  build: {
+    sourcemap: true, // Génère des sourcemaps pour le build
   },
 });
